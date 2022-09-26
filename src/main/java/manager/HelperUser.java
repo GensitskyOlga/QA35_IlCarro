541b89efecdacce03685e8d19b593b6ab7296bf1
GensitskyOlga <gensitskaya13@gmail.com>
2022-09-26 02:11:12 -0700

    error postCondition

---
+++ src/main/java/manager/HelperUser.java
@@ -0,0 +1,60 @@
+package manager;
+
+import models.User;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class HelperUser extends HelperBase{
+
+    public HelperUser(WebDriver wd) {
+        super(wd);
+    }
+
+
+    public void openLoginFormHeader() {
+
+        wd.findElement(By.cssSelector("a[href ^='/login']")).click();
+        // //a[text()=' Log in ']
+    }
+    public void openLoginFormFooter() {
+        // a[href ^='/login']
+        wd.findElement(By.xpath("//a[text()='Log in']")).click();
+    }
+
+    public void fillLoginForm(String email, String password) {
+        type(By.id("email"),email);
+        type(By.id("password"),password);
+    }
+    public void fillLoginForm(User user) {
+        type(By.id("email"), user.getEmail());
+        type(By.id("password"), user.getPassword());
+    }
+
+    public String getMessage() {
+        //pause(3000);
+        WebDriverWait wait = new WebDriverWait(wd, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOf(wd.findElement(By.cssSelector("div.dialog-container"))));
+
+        return wd.findElement(By.cssSelector("h2.message")).getText();
+    }
+
+    public boolean isLogged() {
+        return isElementPresent(By.xpath("//a[text()=' Logout ']"));
+    }
+
+    public void logout() {
+        wd.findElement(By.xpath("//a[text()=' Logout ']")).click();
+    }
+
+    public void clickOkButton() {
+
+        click(By.xpath("//button[text()='Ok']"));
+    }
+}
+
+
+
---
+++ src/test/java/LoginTests.java
@@ -0,0 +1,68 @@
+import models.User;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class LoginTests extends TestBase {
+
+
+    @BeforeMethod
+    public void preCondition() {
+        if (app.getHelperUser().isLogged()) {
+
+            app.getHelperUser().logout();
+        }
+    }
+
+    @Test
+    public void loginSuccess() {
+        app.getHelperUser().openLoginFormHeader();
+        app.getHelperUser().fillLoginForm("gensitskaya@bk.ru", "Od123456$");
+        app.getHelperUser().submit();
+
+        Assert.assertEquals(app.getHelperUser().getMessage(), "Logged in success");
+    }
+
+    @Test
+    public void loginSuccessModels() {
+
+
+        User user = new User().withEmail("gensitskaya@bk.ru").withPassword("Od123456$");
+
+        app.getHelperUser().openLoginFormHeader();
+        app.getHelperUser().fillLoginForm(user);
+        app.getHelperUser().submit();
+
+        Assert.assertEquals(app.getHelperUser().getMessage(), "Logged in success");
+    }
+
+    @Test
+    public void negativeWrongEmail() {
+
+        User user = new User().withEmail("gensitskayabk.ru").withPassword("Od123456$");
+
+        app.getHelperUser().openLoginFormHeader();
+        app.getHelperUser().fillLoginForm(user);
+        app.getHelperUser().submit();
+
+    }
+
+    @Test
+    public void negativeWrongPassword() {
+        User user = new User().withEmail("gensitskaya@bk.ru").withPassword("123456$");
+
+        app.getHelperUser().openLoginFormHeader();
+        app.getHelperUser().fillLoginForm(user);
+        app.getHelperUser().submit();
+        //  Assert.assertEquals(app.getHelperUser().getMessage(),
+        //"Authorization error");
+    }
+
+    @AfterMethod
+    public void postCondition() {
+        app.getHelperUser().clickOkButton();
+    }
+}
