dbe11e819ed8b9c02ce0dccab47ef96fc35a0202
GensitskyOlga <gensitskaya13@gmail.com>
2022-09-27 03:32:42 -0700

    check policy submiy

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -3,63 +3,73 @@ import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+public class LoginTests extends TestBase{
 
-public class LoginTests extends TestBase {
 
 
     @BeforeMethod
-    public void preCondition() {
-        if (app.getHelperUser().isLogged()) {
+    public void preCondition(){
+        if(app.getHelperUser().isLogged())
+            //if(app.getHelperUser().isElementPresent(By.xpath("//a"))){
             app.getHelperUser().logout();
-        }
-    }
 
+
+    }
     @Test
-    public void loginSuccess() {
+    public void loginSuccess(){
         app.getHelperUser().openLoginFormHeader();
-        app.getHelperUser().fillLoginForm("gensitskaya@bk.ru", "Od123456$");
+        app.getHelperUser().fillLoginForm("gensitskaya@bk.ru","Od123456$");
         app.getHelperUser().submit();
-        Assert.assertEquals(app.getHelperUser().getMessage(), "Logged in success");
+
+        Assert.assertEquals(app.getHelperUser().getMessage(),"Logged in success");
     }
 
     @Test
-    public void loginSuccessModels() {
+    public void loginSuccessModels(){
 
-        User user = new User().withEmail("gensitskaya@bk.ru").withPassword("Od123456$");
+
+        User user=new User().withEmail("gensitskaya@bk.ru").withPassword("Od123456$");
+
+        app.getHelperUser().openLoginFormFooter();
+        app.getHelperUser().fillLoginForm(user);
+        app.getHelperUser().submit();
+
+        Assert.assertEquals(app.getHelperUser().getMessage(),"Logged in success");
+    }
+
+    @Test
+    public void negativeWrongEmail(){
+
+        User user=new User().withEmail("gensitskayabk.ru").withPassword("Od123456$");
+
         app.getHelperUser().openLoginFormHeader();
         app.getHelperUser().fillLoginForm(user);
         app.getHelperUser().submit();
-        Assert.assertEquals(app.getHelperUser().getMessage(), "Logged in success");
+        // Assert errorMessagge
+        Assert.assertEquals(app.getHelperUser().getErrorText(),"It'snot look like email");
+        // Assert buttonYalla not active
+        Assert.assertTrue(app.getHelperUser().isYallaButtonNotActive());
     }
-
-    //@Test
-   // public void negativeWrongEmail() {
-
-       // User user = new User().withEmail("gensitskayabk.ru").withPassword("Od123456$");
-       // app.getHelperUser().openLoginFormHeader();
-        //app.getHelperUser().fillLoginForm(user);
-        //app.getHelperUser().submit();
-
-   // }
-
-
-
-
-
     @Test
-    public void negativeWrongPassword() {
-        User user = new User().withEmail("gensitskaya@bk.ru").withPassword("123$");
+    public void negativeWrongPassword(){
+        User user=new User().withEmail("gensitskaya@bk.ru").withPassword("12345$");
+
         app.getHelperUser().openLoginFormHeader();
         app.getHelperUser().fillLoginForm(user);
         app.getHelperUser().submit();
-        Assert.assertEquals(app.getHelperUser().getMessage(), "Wrong email or password");
+        Assert.assertEquals(app.getHelperUser().getMessage(),"Wrong email or password");
+        // Assert text message "Authorization error"
+        Assert.assertEquals(app.getHelperUser().getTitleMessage(),"Authorization error");
 
     }
+
 
     @AfterMethod
-    public void postCondition() {
+    public void postCondition(){
+
         app.getHelperUser().clickOkButton();
     }
 }
---
+++ src/test/java/RegistrationTests.java
@@ -0,0 +1,23 @@
+import models.User;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class RegistrationTests extends TestBase {
+    
+    @BeforeMethod
+    public void preCondition(){
+        if (app.getHelperUser().isLogged())
+            app.getHelperUser().logout();
+    }
+    @Test
+    public void registrationSuccess(){
+        System.out.println(System.currentTimeMillis());
+        int i=(int) (System.currentTimeMillis()/1000)%3600;
+        User user=new User().withName("Nina").withLastname("Ivanov").withEmail("nin"+i+"@gmail.com").withPassword("Nina123$");
+        app.getHelperUser().openRegistrationFormHeader();
+        app.getHelperUser().fillRegistrationForm(user);
+        app.getHelperUser().checkPolicyXY();
+        app.getHelperUser().submit();
+    }
+    
+}
